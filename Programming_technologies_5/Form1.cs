d9e2b0f2fede28178758bc049b5e597943f30a1f
koptelov.kir <koptelov.kir@mail.ru>
2025-03-16 16:18:04 +0800

    Добавьте файлы проекта.

---
+++ Programming_technologies_5/Form1.cs
@@ -0,0 +1,103 @@
+using Programming_technologies_5.Objects;
+
+namespace Programming_technologies_5
+{
+    public partial class Form1 : Form
+    {
+        MyRectangle myRect; // создадим поле под наш пр€моугольник
+        List<BaseObject> objects = new();
+        Player player;
+        Marker marker;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            player = new Player(pbMain.Width / 2, pbMain.Height / 2, 0);
+            player.OnOverlap += (p, obj) =>
+            {
+                txtLog.Text = $"[{DateTime.Now:HH:mm:ss:ff}] »грок пересекс€ с {obj}\n" + txtLog.Text;
+            };
+
+            player.OnMarkerOverlap += (m) =>
+            {
+                objects.Remove(m);
+                marker = null;
+            };
+
+            marker = new Marker(pbMain.Width / 2 + 50, pbMain.Height / 2 + 50, 0);
+            objects.Add(marker);
+            objects.Add(player);
+            objects.Add(new MyRectangle(50, 50, 0));
+            objects.Add(new MyRectangle(100, 100, 45));
+
+            //myRect = new MyRectangle(100, 100, 45); // создать экземпл€р класса
+        }
+
+        private void pbMain_Paint(object sender, PaintEventArgs e)
+        {
+            var g = e.Graphics;
+            g.Clear(Color.White);
+
+            updatePlayer();
+
+            // пересчитываем пересечени€
+            foreach (var obj in objects.ToList())
+            {
+                if (obj != player && player.Overlaps(obj, g))
+                {
+                    player.Overlap(obj);
+                    obj.Overlap(player);
+                }
+            }
+
+            // рендерим объекты
+            foreach (var obj in objects)
+            {
+                g.Transform = obj.GetTransform();
+                obj.Render(g);
+            }
+        }
+        private void updatePlayer()
+        {
+            if (marker != null)
+            {
+                float dx = marker.X - player.X;
+                float dy = marker.Y - player.Y;
+                float length = MathF.Sqrt(dx * dx + dy * dy);
+                dx /= length;
+                dy /= length;
+                player.vX += dx * 0.5f;
+                player.vY += dy * 0.5f;
+                player.Angle = 90 - MathF.Atan2(player.vX, player.vY) * 180 / MathF.PI;
+            }
+
+
+            player.vX += -player.vX * 0.1f;
+            player.vY += -player.vY * 0.1f;
+
+            player.X += player.vX;
+            player.Y += player.vY;
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            pbMain.Invalidate();
+        }
+
+        private void pbMain_MouseClick(object sender, MouseEventArgs e)
+        {
+            // тут добавил создание маркера по клику если он еще не создан
+            if (marker == null)
+            {
+                marker = new Marker(0, 0, 0);
+                objects.Add(marker); // и главное не забыть пололжить в objects
+            }
+
+            // а это так и остаетс€
+            marker.X = e.X;
+            marker.Y = e.Y;
+        }
+    }
+}
---
+++ Programming_technologies_5/Objects/Marker.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programming_technologies_5.Objects
+{
+    class Marker : BaseObject
+    {
+        public Marker(float x, float y, float angle) : base(x, y, angle) { }
+
+        public override void Render(Graphics g)
+        {
+            g.FillEllipse(new SolidBrush(Color.Red), -3, -3, -6, 6);
+            g.DrawEllipse(new Pen(Color.Red, 2), -6, -6, 12, 12);
+            g.DrawEllipse(new Pen(Color.Red, 2), -10, -10, 20, 20);
+
+        }
+        public override GraphicsPath GetGraphicsPath()
+        {
+            // пока возвращаем пустую форму
+            var path = base.GetGraphicsPath();
+            path.AddEllipse(-3, -3, 6, 6);
+            return path;
+        }
+    }
+}
